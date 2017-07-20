001bd39a901df87d5065c6dd06d41da8185b9341
sklaw <271462243@qq.com>
2017-07-21 00:49:23 +0800

    move DMP record to task folder

--- scripts/real_baxter_open_drawer_task/config.py
+++ scripts/real_baxter_open_drawer_task/config.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
+import os
+base_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'DMP_record')
 
-base_path = '/home/sklaw/Desktop/experiment/ros/indigo/baxter_ws/src/birl_baxter_dmp/dmp'
 
+recorded_go_to_start_position_path = os.path.join(base_path, 's1.txt')
+generalized_go_to_start_position_path = os.path.join(base_path, 's1_dmp.txt')
 
-recorded_go_to_start_position_path = base_path+'/open_drawer_datasets/go_to_start_position1.txt'
-generalized_go_to_start_position_path = base_path+'/open_drawer_datasets/go_to_start_position_dmp.txt'
+recorded_go_to_gripper_position_path = os.path.join(base_path, 's2.txt')
+generalized_go_to_gripper_position_path = os.path.join(base_path, 's2_dmp.txt')
 
-recorded_go_to_gripper_position_path = base_path+'/open_drawer_datasets/go_to_gripper_position1.txt'
-generalized_go_to_gripper_position_path = base_path+'/open_drawer_datasets/go_to_gripper_position1_dmp.txt'
+recorded_go_back_path = os.path.join(base_path, 's3.txt')
+generalized_go_back_path = os.path.join(base_path, 's3_dmp.txt')
 
-recorded_go_back_path = base_path+'/open_drawer_datasets/go_back1.txt'
-generalized_go_back_path = base_path+'/open_drawer_datasets/go_back1_dmp.txt'
+recorded_go_forward_path = os.path.join(base_path, "s4.txt")
+generalized_go_forward_path = os.path.join(base_path, "s4_dmp.txt")
 
-recorded_go_forward_path = base_path+"/open_drawer_datasets/go_foreward1.txt"
-generalized_go_forward_path = base_path+"/open_drawer_datasets/go_foreward1_dmp.txt"
-
-recorded_go_back_to_start_position_path = base_path+"/open_drawer_datasets/go_back_to_start_position1.txt"
-generalized_go_back_to_start_position_path = base_path+"/open_drawer_datasets/go_back_to_start_position1_dmp.txt"
+recorded_go_back_to_start_position_path = os.path.join(base_path, "s5.txt")
+generalized_go_back_to_start_position_path = os.path.join(base_path, "s5_dmp.txt")
 
