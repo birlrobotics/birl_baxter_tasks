41576bf3c29470bc616f1031e23f56d2aa801749
rojas70 <rojas70@gmail.com>
2017-09-07 21:07:58 +0800

    add real_baxter_pick_weight_box_and_place_into_cardboard_box task; done pick&place tf frame pub

---
+++ scripts/real_baxter_pick_weight_box_and_place_into_cardboard_box/ar_code_of_boxes_tf_pub.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python  
+import rospy
+from math import pi
+import tf
+import ipdb
+
+if __name__ == '__main__':
+    rospy.init_node('ar_code_of_boxes_tf_pub')
+
+    weight_box_frame = 'ar_marker_1'
+    cardboard_box_frame = 'ar_marker_0'
+    cardboard_depth = 0.15
+    cardboard_center_offset = 0.1
+
+    rate = rospy.Rate(1)
+
+    listener = tf.TransformListener()
+    br = tf.TransformBroadcaster()
+    transformer_ros = tf.TransformerROS()
+
+    while not rospy.is_shutdown():
+        rospy.loginfo('hihi')
+
+        listener.waitForTransform('/base', weight_box_frame, rospy.Time(0), rospy.Duration(10.0))
+        (trans,rot) = listener.lookupTransform('/base', weight_box_frame, rospy.Time(0))
+
+        mat = transformer_ros.fromTranslationRotation(trans,rot)
+        x_axis_vec = mat[:3, 0]
+        y_axis_vec = mat[:3, 1]
+        z_axis_vec = mat[:3, 2]
+
+        mat[:3, 0] = -x_axis_vec 
+        mat[:3, 2] = -z_axis_vec
+        if y_axis_vec[1] < 0:
+            mat[:3, 0] = -x_axis_vec
+            mat[:3, 1] = -y_axis_vec
+
+        br.sendTransform(
+            trans,
+            tf.transformations.quaternion_from_matrix(mat),
+            rospy.Time.now(),
+            "/pick_pose_frame",
+            '/base',
+        )
+
+        listener.waitForTransform('/base', cardboard_box_frame, rospy.Time(0), rospy.Duration(10.0))
+        (trans,rot) = listener.lookupTransform('/base', cardboard_box_frame, rospy.Time(0))
+
+        mat = transformer_ros.fromTranslationRotation(trans,rot)
+        x_axis_vec = mat[:3, 0].copy()
+        y_axis_vec = mat[:3, 1].copy()
+        z_axis_vec = mat[:3, 2].copy()
+
+        mat[:3, 0] = -y_axis_vec 
+        mat[:3, 1] = z_axis_vec
+        mat[:3, 2] = -x_axis_vec
+
+        trans += cardboard_center_offset*mat[:3, 1]+cardboard_depth*mat[:3, 2]
+
+        br.sendTransform(
+            trans,
+            tf.transformations.quaternion_from_matrix(mat),
+            rospy.Time.now(),
+            "/place_pose_frame",
+            '/base',
+        )
+        rate.sleep()
