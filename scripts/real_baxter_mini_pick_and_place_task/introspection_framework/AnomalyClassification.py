477b15c9b2380878c7b0831689fe35fa3f0c072f
rojas70 <rojas70@gmail.com>
2017-11-03 20:06:08 +0800

    will call anomaly classification service now

--- scripts/real_baxter_mini_pick_and_place_task/introspection_framework/AnomalyClassification.py
+++ scripts/real_baxter_mini_pick_and_place_task/introspection_framework/AnomalyClassification.py
@@ -9,6 +9,9 @@ class AnomalyClassification(object):
 
     def classify_anomaly_at(self, anomaly_t):
         sp = rospy.ServiceProxy('AnomalyClassificationService', AnomalyClassificationService)
-        resp = sp(anomaly_t)
-
+        try:
+            resp = sp(anomaly_t)
+        except rospy.ServiceException as exc:
+            rospy.logerr("calling force sensor calibration failed")
+            return False
         return False
