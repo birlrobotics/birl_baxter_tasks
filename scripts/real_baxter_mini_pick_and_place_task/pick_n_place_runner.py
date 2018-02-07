ce7d08d94acee50e8a3ea1837a3285fcd602e9d0
sklaw <sk.law.lsq@gmail.com>
2018-02-08 00:01:03 +0800

    use better introspection interface

--- scripts/real_baxter_mini_pick_and_place_task/pick_n_place_runner.py
+++ scripts/real_baxter_mini_pick_and_place_task/pick_n_place_runner.py
@@ -5,28 +5,17 @@ import rospy
 
 import smach_ros
 
+from smach_based_introspection_framework.online_part import (
+    smach_runner
+)
+
 
 def shutdown():
     pass
 
 if __name__ == '__main__':
-    rospy.init_node("pick_n_place_joint_trajectory")
-    rospy.on_shutdown(shutdown)
-
     from task_states import assembly_user_defined_sm
+
     sm = assembly_user_defined_sm()
-    from smach_based_introspection_framework.interface import modify_user_defined_sm, start_instrospection
 
-    sm = modify_user_defined_sm(sm)
-
-    sis = smach_ros.IntrospectionServer('MY_SERVER', sm, '/SM_ROOT')
-    sis.start()
-
-    start_instrospection(
-        no_anomaly_detection=False,
-        use_manual_anomaly_signal=False,
-    )
-
-    outcome = sm.execute()
-
-    rospy.spin()
+    smach_runner.run(sm)
--- scripts/real_baxter_mini_pick_and_place_task/task_states.py
+++ scripts/real_baxter_mini_pick_and_place_task/task_states.py
@@ -2,7 +2,7 @@ import smach
 import hardcoded_data
 import baxter_interface
 import rospy
-from smach_based_introspection_framework.motion_handler import (
+from smach_based_introspection_framework.online_part.motion_handler import (
     BreakOnAnomalyTrajectoryClient,
 )
 import copy
